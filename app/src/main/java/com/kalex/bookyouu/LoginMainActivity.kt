9969f5aff42445b54832d75001061df8f8a623ec
Kevin Soto <kevinalexandersoto999@gmail.com>
2022-10-23 13:17:53 -0500

    feature : add configuration for Firebase authentication

--- app/src/main/java/com/kalex/bookyouu/LoginMainActivity.kt
+++ app/src/main/java/com/kalex/bookyouu/LoginMainActivity.kt
@@ -8,13 +8,16 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.kalex.bookyouu.databinding.LoginActivityBinding
 
 class LoginMainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: LoginActivityBinding
-
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         // Handle the splash screen transition.
         val splashScreen = installSplashScreen()
@@ -32,8 +35,18 @@ class LoginMainActivity : AppCompatActivity() {
 
         // Keep the splash screen visible for this Activity
         splashScreen.setKeepOnScreenCondition { false }
+        // Initialize Firebase Auth
+        auth = Firebase.auth
     }
-
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) .
+        val currentUser = auth.currentUser
+        if (currentUser != null){
+            //TODO: add the logic to select the user type
+            auth.currentUser?.email
+        }
+    }
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_login)
         return navController.navigateUp(appBarConfiguration)
